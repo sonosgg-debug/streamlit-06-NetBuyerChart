3752fcab5da531a852b34c6be1fccebce7c2686f
SSQ <sonosgg@gmail.com>
2026-09-19 12:47:19 +0900

    UI: update main title font size to 2.0rem

--- app.py
+++ app.py
@@ -245,7 +245,7 @@ def fetch_and_process_data(start_date, end_date, ticker):
 # -----------------------------------------------------------------------------
 # 4. 메인 화면 구성
 # -----------------------------------------------------------------------------
-st.markdown("<h1 class='main-title' style='text-align: center; font-size: 1.9rem !important; font-weight: 800 !important; color: #8AB4F8 !important; -webkit-text-fill-color: #8AB4F8 !important; margin-bottom: 10px;'><span style='color: #8AB4F8 !important; -webkit-text-fill-color: #8AB4F8 !important;'>한국증시 종목 투자자별 매매동향</span></h1>", unsafe_allow_html=True)
+st.markdown("<h1 class='main-title' style='text-align: center; font-size: 2.0rem !important; font-weight: 800 !important; color: #8AB4F8 !important; -webkit-text-fill-color: #8AB4F8 !important; margin-bottom: 10px;'><span style='color: #8AB4F8 !important; -webkit-text-fill-color: #8AB4F8 !important;'>한국증시 종목 투자자별 매매동향</span></h1>", unsafe_allow_html=True)
 st.markdown("<div style='text-align: center; color: #BDC1C6; font-size: 0.9rem; margin-bottom: 20px; line-height: 1.6;'>KRX 거래소 계정 정보를 이용하여 개별 종목의 투자자별 거래대금 추이를 분석합니다.</div>", unsafe_allow_html=True)
 st.markdown("<hr style='border: 0; height: 1px; background-color: #334155; margin-bottom: 22px;'>", unsafe_allow_html=True)
 
